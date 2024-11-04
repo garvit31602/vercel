c826fac800220fe41526b75df672b9f09832178f
unknown <garvit31602@gmail.com>
2024-11-05 00:11:09 +0530

    ff

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+    const name=req.body || 'World'
+    res.send(`Hello ${name}`)
+});
+
+app.post("/api/users/:id",(req,res)=>{
+    res.send(`Hello user ${req.params.id}`)
+})
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
